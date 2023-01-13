6fd181c39f08dec4759e00296e6fefd91a47fe41
codejiangqihan <107793872+codejiangqihan@users.noreply.github.com>
2023-01-13 16:21:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,9 @@
       </div>
       <div class="info">
         <div class="name">iYuan</div>
-        <div class="desc">😊 高一学生、前端小白一枚，未来还请多多指教！</div>
+        <div class="desc">一个似乎啥也不会的人</div>
         <div class="navbar">
-          <a href="https://blog.iyuan.ltd" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-newspaper"></i>博客</a>
-          <a href="https://lab.iyuan.ltd" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-flask"></i>实验室</a>
-          <a href="#" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-comment-dots"></i>即刻</a>
-          <a href="https://blog.iyuan.ltd/about" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-mug-saucer"></i>关于</a>
+          <a href="https://www.codeqihan.com" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-newspaper"></i>博客</a>
         </div>
       </div>
     </div>
@@ -24,24 +21,15 @@
       <div class="contact">
         <h2>📮 联系我</h2>
         <ul>
-          <li>Email：<a href="mailto:yuan@iyuan.ltd">yuan@iyuan.ltd</a></li>
-          <li>Github：<a href="https://github.com/yuanzhidao">@yuanzhidao</a></li>
-          <li>QQ：208823829</li>
-        </ul>
-      </div>
-      <div class="project">
-        <h2>📦 项目</h2>
-        <ul>
-          <li><a href="https://mc-docs.vercel.app" target="_blank" rel="noopener noreferrer">Mc-docs：Minecraft 小白快速入门文档</a></li>
-          <li>（咕咕咕）</li>
-          <li>（咕咕咕）</li>
+          <li>Email：<a href="mailto:qh@1id.top">qh@1id.top</a></li>
+          <li>Github：<a href="https://github.com/codejiangqihan">@codejiangqihan</a></li>
+          <li>QQ：3517889615</li>
         </ul>
       </div>
     </div>
 
     <div class="copyright mt-2 mb-4">
-      <p>© 2022 iYuan</p>
-      <p><a href="https://beian.miit.gov.cn">沪ICP备2022026478号-3</a></p>
+      <p>© 2023 启涵</p>
     </div>
   </div>
 </template>
